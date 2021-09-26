5eb8836c66aa2b4d2ba918f95aa3bfb00ae5330e
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-26 20:37:40 -0300

    Agregando la Cadena de Conexion

--- Api .net Core5/Api/Modelos/Cliente.cs
+++ Api .net Core5/Api/Modelos/Cliente.cs
@@ -4,10 +4,19 @@ namespace Api.Modelos
 {
     public class Cliente
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Nombre { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Apellido { get; set; }
+        [Required]
+        [MaxLength(150)]
         public string Direccion { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Telefono { get; set; }
     }
 }
