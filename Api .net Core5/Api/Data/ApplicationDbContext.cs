b8dcc9bcf8508581efbfc71920eb108f042e205f
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-26 20:41:22 -0300

    Agregando el DbContext

---
+++ Api .net Core5/Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Api.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Cliente> Clientes{get;set;}
+    }
+}
