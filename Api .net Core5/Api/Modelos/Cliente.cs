9d6eb06438bfc1303c6cbb23c6795a9f68d70033
EmilianoBazanZapata <57721403+EmilianoBazanZapata@users.noreply.github.com>
2021-09-26 20:33:21 -0300

    Creando la Clase

---
+++ Api .net Core5/Api/Modelos/Cliente.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Modelos
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
